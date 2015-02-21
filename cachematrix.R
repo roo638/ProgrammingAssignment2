49c561d0644b75c7bef3a18a5749717b71fd5454
roo638 <roo638@gmail.com>
2015-02-21 11:02:56 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,57 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix is the function that set and returns a list of functions
+## It stores a martix and a caches the value of the inverse of the matrix. 
+## This function contains the following subfunctions:
+## *-- set                 :sets the value of a matrix
+## *-- get                 :gets the value of a matrix
+## *-- setMatrixCached     :sets the cached value. This is the inverse of the matrix.
+## *-- getMatrixCached     :gets the cached value. This is the inverse of the matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    cachedMatrix<-NULL
+    
+    set<-function(newValue){
+        x<<-newValue
+        cachedMatrix<<-NULL
+    }
+    
+    get<-function() x
+    
+    setMatrixCached<-function(solve) cachedMatrix<<- solve
+    
+    getMatrixCached<-function() cachedMatrix
+    
+    list(set=set, get=get,
+         setMatrixCached=setMatrixCached,
+         getMatrixCached=getMatrixCached)
+}
 }
 
 
-## Write a short comment describing this function
+## The function below creates the inverse of the matrix(setMatrixCached) 
+## and puts it cache, OR, if already cached, read it from the matrix cache
+## (getMatrixCached)
+## The cacheMatrix variable is used to hold the results of this inverse(solve)
+## function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+cacheSolve <- function(x=matrix(), ...) {
+    cachedMatrix<-x$getMatrixCached()
+    ## Check it the matrix is already cached
+    if(!is.null(cachedMatrix)){
+        message("getting cached data")
+        return(cachedMatrix)
+    }
+    matrix<-x$get()
+    
+    cachedMatrix<-solve(matrix, ...)
+    
+    ## if not cached, put it in the cache here
+    x$setMatrixCached(cachedMatrix)
+    
+    ## cacheMatrix returns a matrix that is the inverse of the input 'x'
+    cachedMatrix
 }
+
